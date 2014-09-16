a5250a4e527ccc2f08bcd0e0f5b5bff8ba17f475
erangeljr <hacker@eddierangel.com>
2014-09-15 21:46:54 -0700

    Initial Check In

---
+++ XamarinUniversity/InstructorAdapter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Android.Widget;
+using Android.App;
+using System.Collections.Generic;
+using Android.Views;
+using System.IO;
+using Android.Graphics.Drawables;
+
+namespace XamarinUniversity
+{
+	public class InstructorAdapter : BaseAdapter<Instructor>
+	{
+		Activity context;
+		List<Instructor> instructors;
+
+
+		public InstructorAdapter(Activity context, List<Instructor> instructors)
+		{
+			this.context = context;
+			this.instructors = instructors;
+		}
+
+		#region implemented abstract members of BaseAdapter
+
+		public override long GetItemId(int position)
+		{
+			return position;
+		}
+
+		public override View GetView(int position, Android.Views.View convertView, Android.Views.ViewGroup parent)
+		{
+			var view = convertView;
+
+			if (view == null)
+			{
+				view = context.LayoutInflater.Inflate(Resource.Layout.InstructorRow, parent, false);
+
+				var p = view.FindViewById<ImageView>(Resource.Id.photoImageView);
+				var n = view.FindViewById<TextView >(Resource.Id.nameTextView);
+				var s = view.FindViewById<TextView >(Resource.Id.specialtyTextView);
+
+				view.Tag = new ViewHolder() { Photo = p, Name = n, Specialty = s };
+			}
+
+			var holder = (ViewHolder)view.Tag;
+
+			holder.Photo.SetImageDrawable(ImageAssetManager.Get(context, instructors[position].ImageUrl));
+			holder.Name     .Text = instructors[position].Name;
+			holder.Specialty.Text = instructors[position].Specialty;
+
+			return view;
+		}
+
+		public override int Count
+		{
+			get
+			{
+				return instructors.Count;
+			}
+		}
+
+		#endregion
+
+		#region implemented abstract members of BaseAdapter
+
+		public override Instructor this[int index]
+		{
+			get
+			{
+				return instructors[index];
+			}
+		}
+
+		#endregion
+
+		// ISectionIndexer implementation
+
+		Java.Lang.Object[]   sectionHeaders        = SectionIndexerBuilder.BuildSectionHeaders       (InstructorData.Instructors);
+		Dictionary<int, int> positionForSectionMap = SectionIndexerBuilder.BuildPositionForSectionMap(InstructorData.Instructors);
+		Dictionary<int, int> sectionForPositionMap = SectionIndexerBuilder.BuildSectionForPositionMap(InstructorData.Instructors);
+
+		public Java.Lang.Object[] GetSections()
+		{
+			return sectionHeaders;
+		}
+
+		public int GetPositionForSection(int section)
+		{
+			return positionForSectionMap[section];
+		}
+
+		public int GetSectionForPosition(int position)
+		{
+			return sectionForPositionMap[position];
+		}
+	
+	}
+}
+
---
+++ XamarinUniversity/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinUniversity
+{
+	[Activity(Label = "Instructors", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			var instructorList = FindViewById<ListView>(Resource.Id.instructorListView);
+			instructorList.FastScrollEnabled = true;
+			instructorList.ItemClick += OnItemClick;
+
+			instructorList.Adapter = new InstructorAdapter(this, InstructorData.Instructors);
+		}
+
+		void OnItemClick(object sender, AdapterView.ItemClickEventArgs e)
+		{
+			var instructor = InstructorData.Instructors[e.Position];
+
+			var dialog = new AlertDialog.Builder(this);
+			dialog.SetMessage(instructor.Name);
+			dialog.SetNeutralButton("OK", delegate {});
+			dialog.Show();
+		}
+	}
+}
+
+
